92e831bb7f19ab9f363b336c6d1486dc8b17185c
71619997a <marksg07@gmail.com>
2017-04-27 14:51:40 -0400

    fib

--- decor.py
+++ decor.py
@@ -36,6 +36,18 @@ def test3(a, b, c, d=5, e=6, f=7):
         a += b + c + d + e + f
     return a
 
+@verbose
+@timer
+def fib(n, cache={0: 0, 1: 1}):
+    print n
+    if n in cache:
+        return cache[n]
+    val = fib(n - 1, cache) + fib(n - 2, cache)
+    cache[n] = val
+    return val
+    
+
 print test2(1, 2, 3, 4, **{'e': 7, 'f':10})
 print test3(1, 2, 3, 4, **{'e': 7, 'f':10})
 print testF(1, 2, 3, 4, **{'e': 7, 'f':10})
+print fib(50)
